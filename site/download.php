5a8b03ee27deebe44d6361f5b570ecd4f798dd03
Iurii <gerojkuki@gmail.com>
2026-05-30 00:31:43 +0100

    Update download.php

--- site/download.php
+++ site/download.php
@@ -69,6 +69,7 @@ require_once 'db.php';
                     <div class="dl-title text-base md:text-xl">Полный клиент World of Tanks v.0.6.5</div>
                     <div class="dl-meta">Размер: ~2.4 ГБ | Формат: ZIP-архив | Версия: 0.6.5 (2011 год)</div>
                     <a href="https://mega.nz/file/XqI2AI5S#4rjABHtHgNhcwbjMhAunvHQXaiCgSSKzLpybNMDGfrU" target="_blank" class="btn btn-primary" style="font-size: 16px; padding: 12px 30px;">Скачать клиент (Mega.nz)</a>
+                    <a href="https://fex.net/ru/s/p0lvm0f" target="_blank" class="btn btn-primary" style="font-size: 16px; padding: 12px 30px; margin-left: 10px;">Скачать клиент (Fex.net)</a>
                 </div>
 
                 <div style="font-size: 18px; font-weight: 600; color: #e5a93b; text-transform: uppercase; margin-top: 30px; margin-bottom: 15px; border-bottom: 1px solid #28282a; padding-bottom: 8px;">Инструкция по установке</div>
